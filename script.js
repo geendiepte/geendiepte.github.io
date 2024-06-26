4d3dd57b9fce9d11d66998528d536c91a1c6f414
geendiepte <milainlambers+geendiepte@gmail.com>
2024-06-27 00:08:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const DEBUG = 0;
+
+function log(x) {
+  if (DEBUG) {
+    console.log(x);
+  }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const button = document.getElementById("press-button");
   const totalPressesDisplay = document.getElementById("total-presses");
@@ -5,6 +13,18 @@ document.addEventListener("DOMContentLoaded", () => {
   const resetCountDisplay = document.getElementById("reset-count");
   const messageDisplay = document.getElementById("message");
   const resetButton = document.getElementById("reset-button");
+
+  const default_fontsize = parseInt(
+    window.getComputedStyle(messageDisplay).fontSize
+  );
+
+  function I(x) {
+    return Math.max(1, x + Math.floor(Math.random() * (3 - -3 + 1)) + -3);
+  }
+
+  const session_reset_sequence = [
+    10,53,5,5,5,5,5,5,5,5,5,5,20,19,18,17,16,15,14,13,3,3,3,3,3,3,1,1,85,50,
+  ].map(I);
 
   const messages = [
     "geen diepte???",
@@ -17,7 +37,7 @@ document.addEventListener("DOMContentLoaded", () => {
     "9.06 single pb",
     "zin in een potje clock??",
     "future team bld champion",
-    "broo synergyyyyyy!!!!!!!!!!!! "
+    "broo synergyyyyyy!!!!!!!!!!!! ",
   ];
 
   let state = "NO_MESSAGE";
@@ -26,6 +46,9 @@ document.addEventListener("DOMContentLoaded", () => {
   let totalPresses = parseInt(localStorage.getItem("totalPresses")) || 0;
   let resetCount = parseInt(localStorage.getItem("resetCount")) || 0;
   let remainingPresses = getRandomPressCount(state);
+
+  let SESSION_SEQUENCE_STARTS_AT = 2; //Math.floor(Math.random() * 10) + 5
+  let session_resets = 0;
 
   button.innerText = presses;
   totalPressesDisplay.innerText = `Total: ${totalPresses}`;
@@ -46,11 +69,67 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   button.addEventListener("click", () => {
-    if (Math.random() * 100 < presses) {
-      presses = 0;
-      resetCount++;
+    /*
+    Rigged press system
+    */
+
+    let update_text = true;
+    if (
+      session_resets >= SESSION_SEQUENCE_STARTS_AT &&
+      session_resets < session_reset_sequence.length
+    ) {
+      if (session_resets == SESSION_SEQUENCE_STARTS_AT) {
+        messageDisplay.style.opacity = 0;
+      }
+      update_text = false;
+      log("rigged press");
+
+      let rigged_reset_num = session_reset_sequence[session_resets];
+
+      log(`sesh resets = ${session_resets} (numindex)`);
+      log(`going to ${rigged_reset_num}`);
+
+      if (rigged_reset_num < presses) {
+        log(`reset!`);
+        remainingPresses++; // to prevent reset on message later without screwing the system
+
+        /* show text and increase fontsize for last 3 messages of sequence
+         */
+        if (
+          session_resets > session_reset_sequence.length - 10 &&
+          session_resets < session_reset_sequence.length - 2
+        ) {
+          messageDisplay.innerText = "Kijk onderin je drop";
+          messageDisplay.style.opacity = 1; // Fade in;
+
+          curr_size = parseInt(
+            window.getComputedStyle(messageDisplay).fontSize
+          );
+          messageDisplay.style.fontSize = curr_size + 5 + "px";
+        } else {
+          if (session_resets == session_reset_sequence.length - 1) {
+            messageDisplay.style.opacity = 0; // Fade Out?
+          }
+        }
+
+        presses = 0;
+        resetCount++;
+        session_resets++;
+      } else {
+        presses++;
+      }
     } else {
-      presses++;
+      log("normal press");
+      /*
+      Normal press system
+      */
+      if (Math.random() * 100 < presses) {
+        presses = 0;
+        resetCount++;
+        session_resets++;
+      } else {
+        presses++;
+      }
     }
 
     totalPresses++;
@@ -70,7 +149,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     remainingPresses--;
 
-    if (remainingPresses <= 0) {
+    if (remainingPresses <= 0 && update_text) {
+      messageDisplay.style.fontSize = default_fontsize + "px";
+
       switch (state) {
         case "NO_MESSAGE":
           state = "SHOWING";
@@ -93,11 +174,16 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
+  /*
+  reset-everything button
+  */
+
   resetButton.addEventListener("click", () => {
     presses = 0;
     maxPresses = 0;
     totalPresses = 0;
     resetCount = 0;
+    session_resets = 0;
 
     button.innerText = presses;
     totalPressesDisplay.innerText = `Total: ${totalPresses}`;
